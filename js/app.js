f1f5cb76636eb609e46e4be828efd9348a143f62
GustavoSchmitt <gustavo.schmitt1998@gmail.com>
2023-10-25 02:03:21 -0300

    responsividade, cep, animação

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
+
+function buscarCEP() {
+  const cep = document.getElementById('inputCEP')
+  if (cep.value.length === 8) {
+      const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+      
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          const inputs = getInputsAddress()
+          inputs.rua.value = `${data.logradouro} ${data.complemento}, ${data.bairro}`
+          inputs.cidade.value = data.localidade
+          inputs.estado.value = data.uf
+            console.log(data);
+      })
+      .catch(error => {
+            console.error('Ocorreu um erro ao buscar o CEP: ' + error);
+      });
+  }
+}
+function getInputsAddress(){
+  return({
+    rua:document.getElementById('inputEndereco'),
+    cidade:document.getElementById('inputCidade'),
+    estado:document.getElementById('inputEstados')
+  })
+}
